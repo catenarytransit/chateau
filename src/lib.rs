53f5a48005623132c7d9a4d88cda9adb8b13915e
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-03-13 16:46:48 -0700

    Handle operatorless static feeds

--- src/lib.rs
+++ src/lib.rs
@@ -96,59 +96,6 @@ pub fn chateau() -> HashMap<String, Chateau> {
 
     let mut chateaus: HashMap<String, Chateau> = HashMap::new();
 
-    let mut counter_single_lords: usize = 0;
-    let mut counter_single_lords_feeds: usize = 0;
-
-    let mut feeds_under_single_lord: HashSet<String> = HashSet::new();
-    let mut operators_single_lord: HashSet<String> = HashSet::new();
-
-    for (operator_id, feed_list) in &dmfr_result.operator_to_feed_hashmap {
-        let mut single_lord_status: bool = true;
-
-        for feed in feed_list.iter() {
-            let feed_id = feed.feed_onestop_id.clone();
-
-            //  println!("{}: {:?}", operator_id, feed);
-
-            let operators_list = dmfr_result.feed_to_operator_pairs_hashmap.get(&feed_id);
-
-            if let Some(operators_list) = operators_list {
-                if operators_list.len() > 1 {
-                    single_lord_status = false;
-
-                    // println!("------\n{:#?}", operators_list);
-                } else {
-                }
-            } else {
-                eprintln!("Unable to find the operators for the feed {}", &feed_id);
-            }
-        }
-
-        if (single_lord_status == true) {
-            counter_single_lords = counter_single_lords + 1;
-            counter_single_lords_feeds = counter_single_lords_feeds + feed_list.len();
-
-            for feed in feed_list.iter() {
-                feeds_under_single_lord.insert(feed.feed_onestop_id.clone());
-                operators_single_lord.insert(operator_id.clone());
-                let chateau_id = name_chateau_from_id(operator_id);
-
-                let single_chateau_result = classify_feed_list(&feed_list, &dmfr_result.feed_hashmap);
-
-                chateaus.insert(chateau_id.clone(), Chateau {
-                    chateau_name: chateau_id.clone(),
-                    realtime_feeds: single_chateau_result.realtime_feeds,
-                    static_feeds: single_chateau_result.static_feeds
-                });
-            }
-        }
-    }
-
-    println!(
-        "{} operators with single lords, a total of {} feeds ",
-        counter_single_lords, counter_single_lords_feeds
-    );
-
     //Perform depth first search
     //visit every tree and mark already visited feeds and operators using a stack and already seen list?
 
@@ -159,7 +106,7 @@ pub fn chateau() -> HashMap<String, Chateau> {
     let mut counter_dfs_cheataus_single_feed: u16 = 0;
 
     for (operator_id, _) in &dmfr_result.operator_to_feed_hashmap {
-        if (!operators_single_lord.contains(operator_id)) && (!already_assigned_operators.contains(operator_id)) {
+        if !already_assigned_operators.contains(operator_id) {
            // println!("{:?}", feed_list);
            // println!("{}", &operator_id);
 
@@ -197,6 +144,37 @@ pub fn chateau() -> HashMap<String, Chateau> {
 
     println!("dfs chateau count: {}", counter_dfs_chateaus);
     println!("dfs chateau with 1 feed: {}", counter_dfs_cheataus_single_feed);
+
+    println!("Assigned a total of {} feeds", already_assigned_feeds.len());
+
+    let mut counter_of_unassigned_feeds: u16 = 0;
+    let mut counter_of_unassigned_rt_feeds: u16 = 0;
+
+    for (feed_id, feed) in dmfr_result.feed_hashmap {
+        if !already_assigned_feeds.contains(&feed_id) {
+           match feed.spec {
+            dmfr::FeedSpec::Gtfs => { 
+              //  println!("{} is GTFS Schedule and not assigned to a Chateau", feed_id);
+              counter_of_unassigned_feeds = counter_of_unassigned_feeds + 1;
+
+                let chateau_id = name_chateau_from_id(&feed_id);
+
+              chateaus.insert(chateau_id.clone(), Chateau {
+                chateau_name:  chateau_id.clone(),
+                realtime_feeds: HashSet::new(),
+                static_feeds: HashSet::from_iter([feed_id])
+            });
+            },
+            dmfr::FeedSpec::GtfsRt => {
+              println!("{} is GTFS Realtime and not assigned to a Chateau", feed_id);
+              counter_of_unassigned_rt_feeds = counter_of_unassigned_rt_feeds + 1;
+            }
+            _ => {}
+           }
+        }
+    }
+
+    println!("Unassigned feed count {} with {} rt", counter_of_unassigned_feeds, counter_of_unassigned_rt_feeds);
 
     chateaus
 }
